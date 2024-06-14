e7ec5c4d214790d7892266d116db5a16c1f72af5
Symbol <lachlanprivate@duck.com>
2024-06-14 19:28:16 +1000

    configure tmux, nvim, and htop

--- setup.sh
+++ setup.sh
@@ -1,2 +1,16 @@
+# ensure nessesary programs are installed
+if ! command -v git &> /dev/null
+then
+    echo "git not found"
+    exit 1
+fi
+
+git clone --filter=blob:none git@github.com:ryanoasis/nerd-fonts ~/fonts/nerd-fonts
+~/fonts/nerd-fonts/install.sh
+
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+
+git clone https://github.com/angels-silhouette/htop ~/.config/htop
+git clone https://github.com/angels-silhouette/tmux ~/.config/tmux
+git clone https://github.com/angels-silhouette/nvim ~/.config/nvim
