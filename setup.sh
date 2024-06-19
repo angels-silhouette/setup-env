dfe984e59732f4c0ec1abcf105442e4e9155d4bd
Symbol <lachlanprivate@duck.com>
2024-06-19 17:52:37 +1000

    prettied the alias stuff

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 # build neovim
 cd 
 git clone https://github.com/neovim/neovim.git
-make CMAKE_BUILD_TYPE=RelWithDebInfo
+make CMAKE_BUILD_TYPE=Release
 sudo make install
 
 # add configs
@@ -29,24 +29,32 @@ git clone https://github.com/angels-silhouette/tmux ~/.config/tmux
 git clone https://github.com/angels-silhouette/nvim ~/.config/nvim
 
 # allow for adding aliases
+echo '# ---------- added by script ---------- #' >> ~/.bashrc
 echo 'if [ -f ~/.bash_aliases ]; then' >> ~/.bashrc
 echo '    . ~/.bash_aliases' >> ~/.bashrc
 echo 'fi' >> ~/.bashrc
+echo '# ------------------------------------- #' >> ~/.bashrc
 
-# editing aliases
+# bash
+echo '# ---------- added by script ---------- #' >> ~/.bash_aliases
 echo 'alias alias-edt="nvim ~/.bash_aliases"' >> ~/.bash_aliases
+echo 'alias history="nvim ~/.bash_history"' >> ~/.bash_aliases
 echo 'alias refresh="source ~/.bashrc"' >> ~/.bash_aliases
+echo '# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #' >> ~/.bash_aliases
 
 # LaTeX aliases
 echo 'alias build="latexmk -pdflua --output-directory=build"' >> ~/.bash_aliases
 echo 'alias delete="rm -rf ./build"' >> ~/.bash_aliases
+echo '# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #' >> ~/.bash_aliases
 
-# QOL aliases
+# quality of life aliases
 echo 'alias grep="grep --color=auto"' >> ~/.bash_aliases
 echo 'alias l="ls -CF"' >> ~/.bash_aliases
 echo 'alias ll="ls -la"' >> ~/.bash_aliases
 echo 'alias ls="ls --color=auto"' >> ~/.bash_aliases
 echo 'alias pub-ip="curl ident.me"' >> ~/.bash_aliases
+echo '# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #' >> ~/.bash_aliases
 
 # neovim aliases
 echo 'alias build-nvim="cd ~/neovim && git pull && make CMAKE_BUILD_TYPE=Release && sudo make install && cd"' >> ~/.bash_aliases
+echo '# ------------------------------------- #' >> ~/.bash_aliases
