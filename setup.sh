ba4fb2aa4e812b2da1ea94c869e443b22ef2826e
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2025-01-14 14:17:54 +1000

    Automatically enter tmux script added to .bashrc

--- setup.sh
+++ setup.sh
@@ -12,21 +12,31 @@ rm -rf ~/.config/tmux-tmp
 git clone https://github.com/angels-silhouette/nvim ~/.config/nvim
 
 # allow for adding aliases
-echo '# ---------- added by script ---------- #' >> ~/.bashrc
-echo 'if [ -f ~/.bash_aliases ]; then' >> ~/.bashrc
-echo '    . ~/.bash_aliases' >> ~/.bashrc
-echo 'fi' >> ~/.bashrc
-echo '# ------------------------------------- #' >> ~/.bashrc
+echo '# ---------- added by script ---------- #
+if command -v tmux > /dev/null; then
+  if [ -z "$TMUX" ]; then
+    if tmux list-sessions > /dev/null 2>&1; then
+      tmux attach-session -d || tmux new-session
+    else
+      tmux new-session
+    fi
+  fi
+fi
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+# ------------------------------------- #' >> ~/.bashrc
 
 # aliases
-echo '############ added by script ############' >> ~/.bash_aliases
-echo '# ---------- quality of life ---------- #' >> ~/.bash_aliases
-echo 'alias alias-edt="nvim ~/.bash_aliases"' >> ~/.bash_aliases
-echo 'alias history="nvim ~/.bash_history"' >> ~/.bash_aliases
-echo 'alias refresh="source ~/.bashrc"' >> ~/.bash_aliases
-echo 'alias grep="grep --color=auto"' >> ~/.bash_aliases
-echo 'alias l="ls -CF"' >> ~/.bash_aliases
-echo 'alias ll="ls -la"' >> ~/.bash_aliases
-echo 'alias ls="ls --color=auto"' >> ~/.bash_aliases
-echo 'alias pub-ip="curl ident.me"' >> ~/.bash_aliases
-echo '#########################################' >> ~/.bash_aliases
+echo '############ added by script ############
+# ---------- quality of life ---------- #
+alias alias-edt="nvim ~/.bash_aliases"
+alias history="nvim ~/.bash_history"
+alias refresh="source ~/.bashrc"
+alias grep="grep --color=auto"
+alias l="ls -CF"
+alias ll="ls -la"
+alias ls="ls --color=auto"
+alias pub-ip="curl ident.me"
+#########################################' >> ~/.bash_aliases
